b45cfdea9dff8904379c68b87a83987e7be58d7b
dmitriy-zverev <dimazverev716@gmail.com>
2025-08-03 13:29:43 +0300

    add print report & sort dict function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, count_characters
+from stats import get_num_words, count_characters, sort_char_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,12 +7,22 @@ def get_book_text(filepath):
     return ""
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    book_path = "books/frankenstein.txt"
     book_contents = get_book_text(book_path)
     num_words = get_num_words(book_contents)
     char_dict = count_characters(book_contents)
+    sorted_char_dict = sort_char_dict(char_dict)
 
-    print(f"{num_words} words found in the document")
-    print(char_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    
+    for char in sorted_char_dict:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+    
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,17 @@ def count_characters(book_contents):
         else:
             char_dict[char] += 1
     return char_dict
+
+def sort_on_num(items):
+    print(items["num"])
+    return items["num"]
+
+def sort_char_dict(char_dict):
+    sorted_char_dict = []
+    
+    for item in char_dict:
+        sorted_char_dict.append({"char": item, "num": char_dict[item]})
+    
+    sorted_char_dict = sorted(sorted_char_dict, reverse=True, key=sort_on_num)
+
+    return sorted_char_dict
